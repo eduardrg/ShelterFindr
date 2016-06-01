ca246ba581e5c4a67b00206ecde381924fa8186f
Brittney <bkhoy@uw.edu>
2016-06-01 01:33:30 -0700

    testing

--- cmd/lab7/main.go
+++ cmd/lab7/main.go
@@ -70,7 +70,7 @@ func main() {
 		table := "<table class='table'><thead><tr>"
 		// put your query here
 
-		rows, err := db.Query("SELECT s.name, a.city FROM shelter s, address a WHERE s.addressId = a.id") // <--- EDIT THIS LINE
+		rows, err := db.Query("SELECT s.name, a.city FROM shelter s, address a WHERE s.addressId = a.id AND a.city=?", 'Seattle') // <--- EDIT THIS LINE
 		if err != nil {
 			// careful about returning errors to the user!
 			c.AbortWithError(http.StatusInternalServerError, err)
