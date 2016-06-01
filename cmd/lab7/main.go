f5e11acbb21a5e9fce82e5e75981b96bb726c9bd
Brittney <bkhoy@uw.edu>
2016-06-01 02:11:18 -0700

    testing

--- cmd/lab7/main.go
+++ cmd/lab7/main.go
@@ -73,7 +73,7 @@ func main() {
 		// }
 
 	router.GET("/query1", func(c *gin.Context) {
-		location := c.Query("location");
+		location := c.Param("location");
 		table := "<table class='table'><thead><tr>"
 		// put your query here
 
