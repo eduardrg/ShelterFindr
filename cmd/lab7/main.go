ab41e96d2cf878928f74b7b248a03398f2ffaf0d
Brittney <bkhoy@uw.edu>
2016-06-01 02:24:30 -0700

    testing

--- cmd/lab7/main.go
+++ cmd/lab7/main.go
@@ -69,6 +69,7 @@ func main() {
 
 	router.GET("/query1", func(c *gin.Context) {
 		location := c.Query("location")
+		log.setOutput(os.Stdout)
 		log.Print(location)
 		table := "<table class='table'><thead><tr>"
 		// put your query here
